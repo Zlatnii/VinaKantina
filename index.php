9e7bae62a6cadce8ade949bfed27cd155462263d
Ivo Zlatunic <zlatunic.ivo@gmail.com>
2023-02-28 21:00:32 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <title>Vjezba HTML</title>
+</head>
+<body>
+        <!--navbar-->
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#"  target="_blank">
+                            <img src="svg/wine-svgrepo-com.svg" alt="" width="30">
+                       </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#"  target="_blank">Web Shop</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#"  target="_blank">Partneri</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" target="_blank">O nama</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                </div>
+            </div>
+        </nav>
+        <div class="container">
+        <!--header-->
+            <div class="header">
+                <h1>Vina Kantina</h1>
+                <p>Talijanska vina, vrhunske kvalitete...</p>
+            </div>
+        <!--bg image -->
+            <div class="background-img-first">
+                <img src="https://assets.bonappetit.com/photos/5beaf49177ffda5e6a77f407/16:9/w_2560%2Cc_limit/ba-ploussard.jpg" width="1000" height="400">
+            </div>
+        </div>
+</body>
+</html>
